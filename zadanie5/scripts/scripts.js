a9730f90b57f337be626d5656e5389824dfc6fc3
Damian Kreft <damiankreft87@gmail.com>
2021-12-06 20:17:09 +0100

    Add exercise 1.2

--- zadanie5/scripts/scripts.js
+++ zadanie5/scripts/scripts.js
@@ -2,8 +2,17 @@ function exercise1_1() {
     addDivFive();
 }
 
+function exercise1_2() {
+    removeFirstDivOccurence();
+}
+
 function addDivFive() {
     let div5 = document.createElement("div");
-    div5 = document.createTextNode("div 5");
+    div5.textContent += "div 5";
     document.body.appendChild(div5);
+}
+
+function removeFirstDivOccurence() {
+    var div = document.body.getElementsByTagName("div")[0];
+    document.body.removeChild(div);
 }
